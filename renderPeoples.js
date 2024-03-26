97f1bab02e81a7a234607032a4e0bef59ed45030
Ligzus <153594049+Ligzus@users.noreply.github.com>
2024-03-26 20:00:09 +0300

    Починил лайки

--- renderPeoples.js
+++ renderPeoples.js
@@ -10,71 +10,80 @@ export function renderPeoples(peoples) {
 
     
     const commentsHtml = peoples
-        .map((people, index) => {
-            return `
-                <li data-index=${index} class="comment">
-                    <div class="comment-header">
-                        <div>${people.name}</div>
-                        <div>${people.time}</div>
+    .map((people, index) => {
+        return `
+            <li data-index=${index} class="comment">
+                <div class="comment-header">
+                    <div>${people.name}</div>
+                    <div>${people.time}</div>
+                </div>
+                <div class="comment-body">
+                    <div class="comment-text">${people.text
+                        .replaceAll("%BEGIN_QUOTE", "<div class='quote'>")
+                        .replaceAll("END_QUOTE%", "</div>")}
                     </div>
-                    <div class="comment-body">
-                        <div class="comment-text">${people.text
-                            .replaceAll("%BEGIN_QUOTE", "<div class='quote'>")
-                            .replaceAll("END_QUOTE%", "</div>")}
-                        </div>
+                </div>
+                <div class="comment-footer">
+                    <div class="likes">
+                        <span class="likes-counter">${people.likes}</span>
+                        <button data-index=${index} class="like-button ${people.isLiked ? 'active-like' : ''}"></button>
                     </div>
-                    <div class="comment-footer">
-                        <div class="likes">
-                            <span class="likes-counter">${people.likes}</span>
-                            <button data-index=${index} class="like-button ${people.isLiked ? 'active-like' : ''}"></button>
-                        </div>
-                    </div>
-                </li>`;
+                </div>
+            </li>`;
+    })
+    .join("");
+
+    const form = `
+    <div id="form-id" class="add-form">
+        <input value=${user ? user.name : ""} readonly id="name" type="text" class="add-form-name" placeholder="Введите ваше имя" />
+        <textarea id="textArea" class="add-form-text" placeholder="Введите ваш коментарий" rows="4"></textarea>
+        <div class="add-form-row">
+            <button id="add-button" class="add-form-button">Написать</button>
+        </div>
+    </div>`;
+
+    const auth = '<p class="auth">Чтобы добавить комментарий, авторизуйтесь<p>';
+    container.innerHTML = `${list} ${user ? form : auth} `;        
+
+    const commentListElement = document.getElementById('commentList');
+    commentListElement.innerHTML = commentsHtml;
+
+    if (!user) {
+        const authButton = document.querySelector('.auth');
+        authButton.addEventListener('click', () => {
+            renderLogin();            
         })
-        .join("");
-
-        const form = `
-        <div id="form-id" class="add-form">
-            <input value=${user ? user.name : ""} readonly id="name" type="text" class="add-form-name" placeholder="Введите ваше имя" />
-            <textarea id="textArea" class="add-form-text" placeholder="Введите ваш коментарий" rows="4"></textarea>
-            <div class="add-form-row">
-                <button id="add-button" class="add-form-button">Написать</button>
-            </div>
-        </div>`;
-
-        const auth = '<p class="auth">Чтобы добавить комментарий, авторизуйтесь<p>';
-        container.innerHTML = `${list} ${user ? form : auth} `;        
-
-        const commentListElement = document.getElementById('commentList');
-        commentListElement.innerHTML = commentsHtml;
-
-        if (!user) {
-            const authButton = document.querySelector('.auth');
-            authButton.addEventListener('click', () => {
-                renderLogin();            
-            })
-        };
-
-
-
-        // Красим кнопку лайка и увеличиваем счетчик
-        for (let button of document.querySelectorAll(".like-button")) {
-        button.addEventListener("click", (event) => { 
-            event.stopPropagation();
-            const index = event.currentTarget.dataset.index;    
-            const currentPeople = peoples[index];                    
-
-            if (currentPeople.isLiked) {
-                currentPeople.likes--;
-            } else {
-                currentPeople.likes++;
-            };
-
-            currentPeople.isLiked = !currentPeople.isLiked;
-
-            renderPeoples(peoples);
-        });
     };
+
+
+
+    // Красим кнопку лайка и увеличиваем счетчик
+    if (user) {
+        for (let button of document.querySelectorAll(".like-button")) {
+            button.addEventListener("click", (event) => { 
+                event.stopPropagation();
+                const index = event.currentTarget.dataset.index;    
+                const currentPeople = peoples[index];                    
+
+                if (currentPeople.isLiked) {
+                    currentPeople.likes--;
+                } else {
+                    currentPeople.likes++;
+                };
+
+                currentPeople.isLiked = !currentPeople.isLiked;
+
+                renderPeoples(peoples);
+            });
+            
+        };
+    } else {
+        for (let button of document.querySelectorAll(".like-button")) {
+            button.removeEventListener("click"); // Удаляем обработчик события клика
+        }
+    };
+
+
 
     // НОВЫЙ КОММЕНТАРИЙ:
 
